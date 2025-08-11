94df6fb61ea357789d501b26e075025970f05d17
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-08-11 09:56:57 -0300

    Update MaoMagica.cs

--- runelanderes/Assets/Scripts/MaoMagica.cs
+++ runelanderes/Assets/Scripts/MaoMagica.cs
@@ -6,6 +6,9 @@ public class MaoMagica : MonoBehaviour
     public InputAction MoveAction;
     public Rigidbody2D rigidbody2d;
     Vector2 move;
+    public int Maxheath = 5;
+    public int currentHeath;
+    public float speed = 0.3f;
     void Start()
     {
         MoveAction.Enable();
@@ -16,10 +19,15 @@ public class MaoMagica : MonoBehaviour
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+        currentHeath = Maxheath;
     }
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHeath (int amount)
+    {
+        currentHeath = Mathf.Clamp(currentHeath + amount, 0, Maxheath); 
     }
 }
